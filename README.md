4f9d2ba34a0627aa86dcb10e2d21b9d511131165
Marco Sangalli <sangalli.marco@gmail.com>
2022-12-16 23:36:42 +0100

    fix: add readme

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ This project was created using create-payload-app using the ts-blank template.
 
 ## How to Use
 
-`yarn dev` will start up your application and reload on any changes.
+`PAYLOAD_SECRET=secret MONGODB_URI=mongodb://localhost:27117/dev yarn dev` will start up your application and reload on any changes.
 
 If you have docker and docker-compose installed, you can run `docker-compose up`
+
+# Dev testing library
+
+Build the lib `yarn build && npm pack`
+Test on the dev project ` cd dev && yarn add ../payload-redis-cache-1.0.0.tgz && PAYLOAD_SECRET=secret MONGODB_URI=mongodb://localhost:27117/dev yarn dev`
--- dev/src/payload.config.ts
+++ dev/src/payload.config.ts
@@ -1,24 +1,45 @@
 import path from 'path'
+import { cachePlugin, extendWebpackConfig } from 'payload-redis-cache'
 import { buildConfig } from 'payload/config'
-// import Examples from './collections/Examples';
-import { cachePlugin } from 'payload-redis-cache'
+import Examples from './collections/Examples'
 import Users from './collections/Users'
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
-    user: Users.slug
+    user: Users.slug,
+    webpack: (webpackConfig) => {
+      webpackConfig.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false
+        }
+      })
+      return {
+        ...webpackConfig,
+        ...extendWebpackConfig
+        // resolve: {
+        //   ...webpackConfig.resolve,
+        //   alias: {
+        //     ...webpackConfig.resolve.alias
+        //     // [adaptersPath]: adaptersMock
+        //   },
+        //   fallback: {
+        //     fs: false,
+        //     net: false,
+        //     crypto: false,
+        //     browser: false
+        //   }
+        // }
+      }
+    }
   },
-  collections: [
-    Users
-    // Add Collections here
-    // Examples,
-  ],
+  collections: [Users, Examples],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts')
   },
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql')
   },
-  plugins: [cachePlugin({ redisURL: '' })]
+  plugins: [cachePlugin({ redisURL: 'redis://localhost:6379' })]
 })
