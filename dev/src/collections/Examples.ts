e104853658b685716b9d99f1aee175f085563c4c
Daniele De Luis <dani.luis92@gmail.com>
2022-12-16 16:55:58 +0100

    fix: docker compose

--- dev/src/collections/Examples.ts
+++ dev/src/collections/Examples.ts
@@ -1,17 +1,17 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig } from 'payload/types'
 
 // Example Collection - For reference only, this must be added to payload.config.ts to be used.
 const Examples: CollectionConfig = {
   slug: 'examples',
   admin: {
-    useAsTitle: 'someField',
+    useAsTitle: 'someField'
   },
   fields: [
     {
       name: 'someField',
-      type: 'text',
-    },
-  ],
+      type: 'text'
+    }
+  ]
 }
 
-export default Examples;
+export default Examples
