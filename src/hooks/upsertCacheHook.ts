cf42a8f3b1b6491e8b14af64fd34afc8d87e7c10
Marco Sangalli <sangalli.marco@gmail.com>
2022-12-20 10:13:58 +0100

    chore: init redis context

--- src/hooks/upsertCacheHook.ts
+++ src/hooks/upsertCacheHook.ts
@@ -1,13 +1,23 @@
-import { CollectionAfterReadHook } from 'payload/types'
+import { CollectionAfterReadHook, PayloadRequest } from 'payload/types'
 import { setCacheItem } from '../helpers'
 import { DEFAULT_USER_COLLECTION } from '../types'
+
+// internal user lookup has a cookie but not an user
+export const isInternalLookup = (req: PayloadRequest) => {
+  const {
+    headers: { cookie },
+    user
+  } = req
+  return !user && cookie
+}
 
 export const upsertCacheHook: CollectionAfterReadHook = ({ doc, req }) => {
   const { originalUrl, user } = req
   const userCollection = user ? user.collection : DEFAULT_USER_COLLECTION
 
   // Keep the original URL no split needed
-  setCacheItem(userCollection, originalUrl, doc)
-
+  if (!isInternalLookup(req)) {
+    setCacheItem(userCollection, originalUrl, doc)
+  }
   return doc
 }
