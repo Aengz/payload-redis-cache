2acdf15d3ebfb1b4cd3f7d81ac7bfc69d93e3880
Marco Sangalli <sangalli.marco@gmail.com>
2023-01-27 12:11:19 +0100

    chore: add cache

--- dev/src/payload.config.ts
+++ dev/src/payload.config.ts
@@ -7,26 +7,7 @@ import Users from './collections/Users'
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
-    user: Users.slug,
-    webpack: (webpackConfig) => {
-      console.log(webpackConfig)
-      webpackConfig.module.rules.push({
-        test: /\.m?js/,
-        resolve: {
-          fullySpecified: false
-        }
-      })
-      return {
-        ...webpackConfig,
-        resolve: {
-          ...(webpackConfig.resolve || {}),
-          alias: {
-            ...(webpackConfig.resolve.alias || {}),
-            react: path.resolve(__dirname, '../node_modules/react')
-          }
-        }
-      }
-    }
+    user: Users.slug
   },
   collections: [Users, Examples],
   typescript: {
