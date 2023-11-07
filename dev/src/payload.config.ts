781a5ca64fcb2b1169460013deaaeaebef437ee9
Daniele De Luis <dani.luis92@gmail.com>
2023-11-07 22:04:30 +0100

    update: dev environment

--- dev/src/payload.config.ts
+++ dev/src/payload.config.ts
@@ -1,15 +1,23 @@
 import { cachePlugin } from '@aengz/payload-redis-cache'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { slateEditor } from '@payloadcms/richtext-slate'
 import path from 'path'
-import { buildConfig } from 'payload/config'
+import { Config, buildConfig } from 'payload/config'
 import Examples from './collections/Examples'
 import Users from './collections/Users'
 
-export default buildConfig({
+const config: Config = {
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
     user: Users.slug
   },
+  bundler: webpackBundler(),
   collections: [Users, Examples],
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI || ''
+  }),
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts')
   },
@@ -19,4 +27,6 @@ export default buildConfig({
   plugins: [
     cachePlugin({ excludedCollections: ['users'] }) // ADD HERE
   ]
-})
+}
+
+export default buildConfig(config)
